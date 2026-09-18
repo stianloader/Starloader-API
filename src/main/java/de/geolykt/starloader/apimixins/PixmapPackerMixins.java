dc263484de80e4afc4624564fb2850ba35d699ca
Geolykt <admin@geolykt.de>
2026-09-18 14:29:21 +0200

    Actually make the pixmap packer additions work
    The mixin was never registered which completely shadowed
    the fact that the MethodHandles did not work at all.
    Now that it has been tested those flaws have been uncovered.
    Still not tested on libGDX 1.9.11 but it should at the very least run
    as long as the ARGB8888 pack method is not invoked.
    
    I could swear I registered that mixin … but I was probably distracted just before
    actually registering it. I for sure have ADHD of some kind but it is
    a PITA to get it diagnosed in Germany and honestly it isn't /that/
    much of a hinderance so /shrug.

--- src/main/java/de/geolykt/starloader/apimixins/PixmapPackerMixins.java
+++ src/main/java/de/geolykt/starloader/apimixins/PixmapPackerMixins.java
@@ -21,6 +21,7 @@ import com.badlogic.gdx.graphics.g2d.PixmapPacker.PackStrategy;
 import com.badlogic.gdx.graphics.g2d.PixmapPacker.Page;
 import com.badlogic.gdx.graphics.g2d.PixmapPacker.PixmapPackerRectangle;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 import de.geolykt.starloader.impl.JavaInterop;
 import de.geolykt.starloader.impl.gui.s2d.PixmapPackerExtensions;
@@ -45,6 +46,26 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
     @NotNull
     private static final MethodHandle SLAPI$MH_SET_RECT_PAGE;
 
+    @Unique
+    @NotNull
+    private static final MethodHandle SLAPI$MH_PIXMAP_RECT_GET_WIDTH;
+
+    @Unique
+    @NotNull
+    private static final MethodHandle SLAPI$MH_PIXMAP_RECT_GET_HEIGHT;
+
+    @Unique
+    @NotNull
+    private static final MethodHandle SLAPI$MH_PIXMAP_RECT_GET_X;
+
+    @Unique
+    @NotNull
+    private static final MethodHandle SLAPI$MH_PIXMAP_RECT_GET_Y;
+
+    @Unique
+    @NotNull
+    private static final MethodHandle SLAPI$MH_PAGE_ADD_RECT_NAME;
+
     static {
         MethodHandles.Lookup lookup = MethodHandles.lookup();
 
@@ -59,14 +80,14 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         try {
             // Old libGDX
             MethodHandle mhPackStratPack = lookup.findVirtual(PackStrategy.class, "pack", MethodType.methodType(Page.class, PixmapPacker.class, String.class, Rectangle.class));
-            slapi$mhPackStrategyPack = mhPackStratPack.asType(MethodType.methodType(Page.class, PixmapPacker.class, String.class, PixmapPackerRectangle.class));
+            slapi$mhPackStrategyPack = mhPackStratPack.asType(MethodType.methodType(Page.class, PackStrategy.class, PixmapPacker.class, String.class, PixmapPackerRectangle.class));
         } catch (NoSuchMethodException e1) {
             // New libGDX
             try {
                 Class<?> bounds = Class.forName("com.badlogic.gdx.graphics.g2d.PixmapPacker$Bounds", false, PixmapPackerMixins.class.getClassLoader());
                 MethodHandle mhGetBounds = lookup.findGetter(PixmapPackerRectangle.class, "bounds", bounds);
                 MethodHandle mhPackStratPack = lookup.findVirtual(PackStrategy.class, "pack", MethodType.methodType(Page.class, PixmapPacker.class, String.class, bounds));
-                slapi$mhPackStrategyPack = MethodHandles.filterArguments(mhPackStratPack, 2, mhGetBounds);
+                slapi$mhPackStrategyPack = MethodHandles.filterArguments(mhPackStratPack, 3, mhGetBounds);
             } catch (ReflectiveOperationException e2) {
                 e2.addSuppressed(e1);
 
@@ -79,7 +100,7 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         SLAPI$MH_PACK_STRATEGY_PACK = slapi$mhPackStrategyPack;
 
         try {
-            SLAPI$MH_MAKE_PAGE_DIRTY = lookup.findSetter(Page.class, "dirty", boolean.class);
+            SLAPI$MH_MAKE_PAGE_DIRTY = MethodHandles.insertArguments(lookup.findSetter(Page.class, "dirty", boolean.class), 1, true);
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_MAKE_PAGE_DIRTY)", e);
         }
@@ -97,6 +118,92 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         }
 
         SLAPI$MH_SET_RECT_PAGE = mhSetPageRect;
+
+        MethodHandle mhFloat2Int;
+
+        try {
+            mhFloat2Int = lookup.findVirtual(Float.class, "intValue", MethodType.methodType(int.class)).asType(MethodType.methodType(int.class, float.class));
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (mhFloat2Int)", e);
+        }
+
+        MethodHandle mhPixmapRectGetWidth;
+        MethodHandle mhPixmapRectGetHeight;
+        MethodHandle mhPixmapRectGetX;
+        MethodHandle mhPixmapRectGetY;
+
+        try {
+            mhPixmapRectGetWidth = lookup.findVirtual(PixmapPackerRectangle.class, "getWidth", MethodType.methodType(float.class));
+            mhPixmapRectGetWidth = MethodHandles.filterReturnValue(mhPixmapRectGetWidth, mhFloat2Int);
+        } catch (NoSuchMethodException e1) {
+            try {
+                mhPixmapRectGetWidth = lookup.findVirtual(PixmapPackerRectangle.class, "getWidth", MethodType.methodType(int.class));
+            } catch (ReflectiveOperationException e2) {
+                e2.addSuppressed(e1);
+
+                throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_WIDTH): Fallbacks exhausted", e2);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_WIDTH): No access", e);
+        }
+
+        try {
+            mhPixmapRectGetHeight = lookup.findVirtual(PixmapPackerRectangle.class, "getHeight", MethodType.methodType(float.class));
+            mhPixmapRectGetHeight = MethodHandles.filterReturnValue(mhPixmapRectGetHeight, mhFloat2Int);
+        } catch (NoSuchMethodException e1) {
+            try {
+                mhPixmapRectGetHeight = lookup.findVirtual(PixmapPackerRectangle.class, "getHeight", MethodType.methodType(int.class));
+            } catch (ReflectiveOperationException e2) {
+                e2.addSuppressed(e1);
+
+                throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_HEIGHT): Fallbacks exhausted", e2);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_HEIGHT): No access", e);
+        }
+
+        try {
+            mhPixmapRectGetX = lookup.findVirtual(PixmapPackerRectangle.class, "getX", MethodType.methodType(float.class));
+            mhPixmapRectGetX = MethodHandles.filterReturnValue(mhPixmapRectGetX, mhFloat2Int);
+        } catch (NoSuchMethodException e1) {
+            try {
+                mhPixmapRectGetX = lookup.findVirtual(PixmapPackerRectangle.class, "getX", MethodType.methodType(int.class));
+            } catch (ReflectiveOperationException e2) {
+                e2.addSuppressed(e1);
+
+                throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_X): Fallbacks exhausted", e2);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_X): No access", e);
+        }
+
+        try {
+            mhPixmapRectGetY = lookup.findVirtual(PixmapPackerRectangle.class, "getY", MethodType.methodType(float.class));
+            mhPixmapRectGetY = MethodHandles.filterReturnValue(mhPixmapRectGetY, mhFloat2Int);
+        } catch (NoSuchMethodException e1) {
+            try {
+                mhPixmapRectGetY = lookup.findVirtual(PixmapPackerRectangle.class, "getY", MethodType.methodType(int.class));
+            } catch (ReflectiveOperationException e2) {
+                e2.addSuppressed(e1);
+
+                throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_Y): Fallbacks exhausted", e2);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PIXMAP_RECT_GET_Y): No access", e);
+        }
+
+        SLAPI$MH_PIXMAP_RECT_GET_WIDTH = mhPixmapRectGetWidth;
+        SLAPI$MH_PIXMAP_RECT_GET_HEIGHT = mhPixmapRectGetHeight;
+        SLAPI$MH_PIXMAP_RECT_GET_X = mhPixmapRectGetX;
+        SLAPI$MH_PIXMAP_RECT_GET_Y = mhPixmapRectGetY;
+
+        try {
+            MethodHandle mhGetRectNames = lookup.findGetter(Page.class, "addedRects", Array.class);
+            MethodHandle mhArrayAdd = lookup.findVirtual(Array.class, "add", MethodType.methodType(void.class, Object.class)).asType(MethodType.methodType(void.class, Array.class, String.class));
+            SLAPI$MH_PAGE_ADD_RECT_NAME = MethodHandles.filterArguments(mhArrayAdd, 0, mhGetRectNames);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("SLAPI: Cannot generate compatibility shims (SLAPI$MH_PAGE_ADD_RECT_NAME)", e);
+        }
     }
 
     @Shadow
@@ -124,7 +231,7 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         PixmapPackerRectangle rect;
 
         try {
-            rect = (PixmapPackerRectangle) PixmapPackerMixins.SLAPI$MH_CREATE_PIXMAP_RECT.invokeExact(0, 0, imageWidth, imageHeight);
+            rect = (@NotNull PixmapPackerRectangle) PixmapPackerMixins.SLAPI$MH_CREATE_PIXMAP_RECT.invokeExact(0, 0, imageWidth, imageHeight);
         } catch (Throwable t) {
             if (t instanceof Error) {
                 throw (Error) t;
@@ -137,14 +244,33 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
             throw new RuntimeException(t);
         }
 
-        if (rect.getWidth() > this.pageWidth || rect.getHeight() > this.pageHeight) {
+        int rectW, rectH, rectX, rectY;
+
+        try {
+            rectW = (int) PixmapPackerMixins.SLAPI$MH_PIXMAP_RECT_GET_WIDTH.invokeExact(rect);
+            rectH = (int) PixmapPackerMixins.SLAPI$MH_PIXMAP_RECT_GET_HEIGHT.invokeExact(rect);
+            rectX = (int) PixmapPackerMixins.SLAPI$MH_PIXMAP_RECT_GET_X.invokeExact(rect);
+            rectY = (int) PixmapPackerMixins.SLAPI$MH_PIXMAP_RECT_GET_Y.invokeExact(rect);
+        } catch (Throwable t) {
+            if (t instanceof Error) {
+                throw (Error) t;
+            } else if (t instanceof RuntimeException) {
+                throw (RuntimeException) t;
+            } else if (t instanceof IOException) {
+                throw new UncheckedIOException((IOException) t);
+            }
+
+            throw new RuntimeException(t);
+        }
+
+        if (rectW > this.pageWidth || rectH > this.pageHeight) {
             throw new IllegalArgumentException("Page size too small for rectangle: " + path + "; Rectangle dimensions: " + imageWidth + "x" + imageHeight);
         }
 
         Page page;
 
         try {
-            page = (Page) PixmapPackerMixins.SLAPI$MH_PACK_STRATEGY_PACK.invokeExact(this, path, rect);
+            page = (Page) PixmapPackerMixins.SLAPI$MH_PACK_STRATEGY_PACK.invokeExact(this.packStrategy, this, path, rect);
         } catch (Throwable t) {
             if (t instanceof Error) {
                 throw (Error) t;
@@ -158,8 +284,8 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         }
 
         ByteBuffer destinationAddress = Objects.requireNonNull(page.getPixmap().getPixels(), "The page's pixel buffer may not be null");
-        long dstOffset = (long) rect.getX() * 4;
         long dstStride = page.getPixmap().getWidth() * 4;
+        long dstOffset = (long) rectX * 4 + (long) rectY * dstStride;
 
         while (imageHeight-- != 0) {
             MemoryUtil.copyARGB8888ToRGBA8888(bufferAddress, destinationAddress, dstOffset, imageWidth);
@@ -168,6 +294,9 @@ public class PixmapPackerMixins implements PixmapPackerExtensions {
         }
 
         try {
+            PixmapPackerMixins.SLAPI$MH_PAGE_ADD_RECT_NAME.invokeExact(page, path);
+            page.getRects().put(path, rect);
+
             PixmapPackerMixins.SLAPI$MH_MAKE_PAGE_DIRTY.invokeExact(page);
             PixmapPackerMixins.SLAPI$MH_SET_RECT_PAGE.invokeExact(rect, page);
         } catch (Throwable t) {
